9c7fa11e835e638de868e0472cf7588a3b194400
028-ana <anaclara.evg@gmail.com>
2025-11-27 16:02:33 -0300

    Add files via upload

--- core/models.py
+++ core/models.py
@@ -3,20 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pontos = models.IntegerField(default=0)
-    
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='core_user_set',  # muda o related_name para evitar conflito
-        blank=True,
-        help_text='The groups this user belongs to.'
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='core_user_permissions_set',  # muda o related_name
-        blank=True,
-        help_text='Specific permissions for this user.'
-    )
-
 
 class DropOffPoint(models.Model):
     nome = models.CharField(max_length=100)
@@ -35,6 +21,7 @@ class Reward(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.points_required} pontos)"
+
 class Submission(models.Model):
     STATUS_CHOICES = (
         ('pending', 'Pendente'),
@@ -48,7 +35,7 @@ class Submission(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="pending")
     points_awarded = models.PositiveIntegerField(default=0)
-    validation_message = models.TextField(blank=True, null=True)  # NOVO CAMPO
+    validation_message = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.created_at.strftime('%d/%m/%Y')}"
